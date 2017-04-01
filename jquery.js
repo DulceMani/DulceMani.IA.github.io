8d24ea4a537c2df6d91b9b510c0123889cdbe564
DulceMani <dulcenataly26@gmail.com>
2017-03-31 21:25:32 -0600

    Se creo la matriz de la interfaz principal Se agrego la biblioteca de jquery

--- jquery.js
+++ jquery.js
@@ -1,3 +1,49 @@
 $(document).ready(function(){
-    
+    var columnas = 10;
+    var renglones = 10;
+    var index = 0;
+    var j,i
+
+    creaMatriz()
+
+    function creaMatriz(){
+        for(i=0;i<renglones;i++)
+        {
+            for(j=0;j<columnas-1;j++)
+            {
+                agregaColumna(i,j)
+            }
+            agregaRenglon(i,0)
+        } 
+    }
+    function agregaColumna(i,j){
+        var cid = '#'+i+'c'+j;
+        var $td = $('<td>')
+        $(cid).after($td)
+        var newid = i+'c'+(j+1)
+        $td.attr('id',newid)
+        $td.append('<div>')
+        $('div').addClass('circulo');
+        
+    }
+    function agregaRenglon(i,j){
+        var rid = '#r'+i
+        var $tr = $('<tr>')
+        $(rid).after($tr)
+        var newid ='r'+(i+1)
+        $tr.attr('id',newid)
+        if(i!= renglones-1)
+        agregaPrimerColumna($tr,i,j)
+    }
+    function agregaPrimerColumna($tr,i,j){
+        var cid = '#'+i+'c'+j;
+        var $td = $('<td>')
+        $tr.append($td)
+        var newid = (i+1)+'c'+j
+        $td.attr('id',newid)
+        $td.append('<div>')
+        $('div').addClass('circulo');
+        
+    }
+
 })
