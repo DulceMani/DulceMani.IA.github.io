e88c6449cca5eb4c1b7fb0c031c1a6655032ff15
rudoxvanx <rudoxvanx@gmail.com>
2017-05-01 12:50:14 -0500

    Interaccion con el gato

--- jquery.js
+++ jquery.js
@@ -2,20 +2,23 @@ $(document).ready(function(){
     var columnas = 5;
     var renglones = 5;
     var index = 0;
-    var j,i
+    var j,i;
+    var x= renglones/2;
+    var y= columnas/2;
+
    //var $img = $('<img id="theCat"  src="Sr._Gato.png" />')
     creaMatriz()
     agrega_al_gatito()
     function agrega_al_gatito()
-    {
-        var x= renglones/2;
-        var y= columnas/2;
+    {   
+        var idcIni; 
+        //Idea para empezar la interaccion
         x= parseInt(x);
         y= parseInt(y);
-        var idc = '#'+x+'c'+y;
+        idcIni = '#'+x+'c'+y;
         //var div=  document.getElementById(idc).children[0]
         //div.innerHTML= $img
-        var $div = $(idc).children('div')
+        var $div = $(idcIni).children('div')
         $div.prepend($('<img>',{id:'theCat',src:'Sr._Gato.png'}));
     }
     function creaMatriz(){
@@ -57,6 +60,134 @@ $(document).ready(function(){
         $('div').addClass('circulo');
         
     }
+    
+    function getAleatorio() {
+        
+         return Math.floor((Math.random() * 4) + 1);
+    }
+
+    function getXActual(){
+      return x;
+    }
+
+    function getYActual(){
+       return y;
+    }
+
+    function actualizacion(dir){
+
+        switch(dir){
+
+            case 1:
+                if(getXActual()>0)
+                   x--;
+            break; 
+
+            case 2:
+                if(getXActual()<4)
+                   x++;
+            break;   
+
+            case 3:
+                if(getYActual()>0)
+                   y--;
+            break;   
+
+            case 4:
+                if(getXActual()<4)
+                   y++;
+            break;     
+        }
+  
+    }
+
+    function turnoCat(){
+       var Dire;
+       var idO;
+       var $div1;
+       var $div2;
+       var idN;
+       var co;
+       var Cor;
+       
+    
+            Dire=getAleatorio();
+            
+            if(Dire==1){
+                    Cor=x;
+                    Cor--;
+                    idO='#'+Cor+'c'+y;
+                    idN='#'+x+'c'+y;
+                    $div1= $(idO).children('div');
+                    co = ($div1).css('backgroundColor');
+        
+                    
+
+                    if(co='purple'){  
+                        //$div2= $(idN).children('div')
+                        $("#theCat").remove()
+                        $div1.prepend($('<img>',{id:'theCat',src:'Sr._Gato.png'})); 
+                        actualizacion(Dire)
+                    } 
+                    
+            }
+
+            if(Dire==2){
+                    Cor=x;
+                    Cor++;
+                    idO='#'+Cor+'c'+y;
+                    idN='#'+x+'c'+y;
+                    $div1= $(idO).children('div');
+                    co = ($div1).css('backgroundColor');
+                  
+
+                    if(co='purple'){  
+                        //$div2= $(idN).children('div')
+                        $("#theCat").remove()
+                        $div1.prepend($('<img>',{id:'theCat',src:'Sr._Gato.png'})); 
+                        actualizacion(Dire)
+                    } 
+                    
+            }
+
+                if(Dire==3){
+                    Cor=y;
+                    Cor--;
+                    idO='#'+x+'c'+Cor;
+                    idN='#'+x+'c'+y;
+                    $div1= $(idO).children('div');
+                    co = ($div1).css('backgroundColor');
+        
+
+                    if(co='purple'){  
+                        //$div2= $(idN).children('div')
+                        $("#theCat").remove()
+                        $div1.prepend($('<img>',{id:'theCat',src:'Sr._Gato.png'})); 
+                        actualizacion(Dire)
+                    } 
+                    
+            }
+
+                if(Dire==4){
+                    Cor=y;
+                    Cor++;
+                    idO='#'+Cor+'c'+y;
+                    idN='#'+x+'c'+y;
+                    $div1= $(idO).children('div');
+                    co = ($div1).css('backgroundColor');
+
+                    if(co='purple'){  
+                        //$div2= $(idN).children('div')
+                        $("#theCat").remove()
+                        $div1.prepend($('<img>',{id:'theCat',src:'Sr._Gato.png'}));
+                        actualizacion(Dire) 
+                    } 
+                    
+            }
+       
+
+    }
+
     $(function(){
         $('.circulo').click(function(){
             
@@ -64,8 +195,11 @@ $(document).ready(function(){
             var idc = $img.attr('id')
             if(idc != 'theCat')
             {
-                $(this).css('background-color','blue');
+                $(this).css('background-color','blue')
+                 turnoCat();
             }
+            
+           
         })
       })
 })
